1649edca88cb5f96f9c601f0248922cfccc18ffc
DORA_XIE <joy8963259@gmail.com>
2025-01-07 19:04:27 +0800

    chore: update tron to 6.0

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,36 @@ const webpack = require('webpack');
 const mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
-    entry: './index.js',
-    devtool: 'source-map',
-    target: 'web',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [ '@babel/preset-env' ],
-                        plugins: [ '@babel/plugin-transform-runtime', '@babel/plugin-transform-modules-commonjs' ],
-                    }
-                }
-            }
-        ]
-    },
-    resolve: {
-        modules: [ '../../node_modules' ]
-    },
-    plugins: [
-        new webpack.optimize.ModuleConcatenationPlugin(),
-        new webpack.DefinePlugin({
-            ENVIRONMENT: JSON.stringify(mode)
-        })
+  entry: './index.js',
+  devtool: 'source-map',
+  target: 'web',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules\/(?!(@noble|ethereum-cryptography)\/).*/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              '@babel/plugin-syntax-bigint',
+              '@babel/plugin-proposal-optional-chaining',
+              '@babel/plugin-transform-numeric-separator',
+            ],
+          },
+        },
+      },
     ],
-    mode
+  },
+  resolve: {
+    modules: ['../../node_modules'],
+  },
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.DefinePlugin({
+      ENVIRONMENT: JSON.stringify(mode),
+    }),
+  ],
+  mode,
 };
